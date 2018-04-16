351f1c1d28d0c7957eedf4ed4de89da5355d9cf7
Blanc <zlj5676@gmail.com>
2018-04-16 14:12:43 +0800

    EX14

---
+++ EX14/app/src/main/java/com/example/administrator/ex14/Intent_Hiden.java
@@ -0,0 +1,17 @@
+package com.example.administrator.ex14;
+
+/**
+ * Created by Administrator on 2018/4/16 0016.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Intent_Hiden extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_intent);
+
+    }
+}
